94102e7481bd34c4f0320720d3c2448553d4db92
Jacob Zhong <cmpute@qq.com>
2022-05-14 11:32:45 -0400

    Add gcd example

---
+++ examples/gcd_profile.rs
@@ -0,0 +1,50 @@
+use ibig::{
+    ops::Gcd,
+    ubig, UBig,
+};
+use rand::prelude::*;
+
+fn random_ubig<R>(bits: usize, rng: &mut R) -> UBig
+where
+    R: Rng + ?Sized,
+{
+    rng.gen_range(ubig!(1) << (bits - 1)..ubig!(1) << bits)
+}
+
+pub(crate) fn egcd(mut a: UBig, mut b: UBig) -> UBig {
+    // find common factors of 2
+    let shift = (&a | &b).trailing_zeros().unwrap();
+    a >>= a.trailing_zeros().unwrap();
+    b >>= b.trailing_zeros().unwrap();
+
+    // the binary GCD algorithm
+    while a != b {
+        if &a > &b {
+            a -= &b;
+            a >>= a.trailing_zeros().unwrap();
+        } else {
+            b -= &a;
+            b >>= b.trailing_zeros().unwrap();
+        }
+    }
+    a << shift
+}
+
+fn bench() {
+    let mut s = 0;
+    let mut rng = StdRng::seed_from_u64(1);
+    let bits = 10usize.pow(5);
+    for _ in 0..4 {
+        let a = random_ubig(bits, &mut rng);
+        let b = random_ubig(bits, &mut rng);
+        // let g = a.gcd(b);
+        let g = egcd(a, b);
+        s += g.trailing_zeros().unwrap();
+    }
+
+    println!("sum trailing zeros: {}", s);
+}
+
+fn main() {
+    bench()
+}
